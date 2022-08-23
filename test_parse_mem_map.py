9b073bfd8f7e49b0782747f401a984630b098d8e
Shone <marsatwar@gmail.com>
2022-08-23 06:30:09 +0000

    Updated and renamed parse_mem_map.py

---
+++ test_parse_mem_map.py
@@ -0,0 +1,58 @@
+# Author: lindafang
+# Date: 2020-10-05 15:00
+# File: test_attach_html_png.py
+import allure
+import os
+
+def Get_specified_field(cmd_str):
+    result = os.popen(cmd_str)
+    field_str = ""
+    # 返回的结果是一个<class 'os._wrap_close'>对象，需要读取后才能处理
+    context = result.read()
+    #print("\n" + context + "sad")
+    #return (context)
+    for line in context.splitlines():
+        field_str = line
+        break
+        #print(line)
+    result.close()
+    print("\n" + field_str)
+    return field_str
+
+def test_multiple_attachments():
+    file_name = Get_specified_field("ls *.map")
+    field_prog = 'PROGRAM_FLASH'
+
+    prog_start_addr = Get_specified_field("""
+    cat {} | grep __top_PROGRAM_FLASH | cut -d '=' -f 2 | awk '{{$1=$1}};1'
+    """.format(file_name))
+
+    prog_end_addr= 'TBD'
+    prog_size= 'TBD'
+    prog_free= 'TBD'
+    prog_used= 'TBD'
+    prog_usage= 'TBD'
+
+    allure.attach("""<head></head><body> XMicro1 Memory Map Table
+        </body>
+        <h1>Data acquired from *.map</h1>
+        <table  style="border:3px #cccccc solid;" width="400" cellpadding="10" border='1'>
+          <tr>
+            <th width="200">Region</th>
+            <th width="200">Start address</th>
+            <th width="200">End address</th>
+            <th width="200">Size</th>
+            <th width="200">Free</th>
+            <th width="200">Used</th>
+            <th width="200">Usage</th>
+          </tr>
+          <tr align="center">
+            <td width="200">{}</th>
+            <td width="200">{}</th>
+            <td width="200">{}</th>
+            <td width="200">{}</th>
+            <td width="200">{}</th>
+            <td width="200">{}</th>
+            <td width="200">{}</th>         
+          </tr>
+        </table>""".format(prog_start_addr, prog_end_addr, prog_size, prog_free, prog_used, prog_usage), '擷取build code後的文檔數據', allure.attachment_type.HTML)
